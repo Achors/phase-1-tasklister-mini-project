b4bc548ab5d1cd3ae1de738e31c4921cbc9b9291
Adolf <adolfnyerere@gmail.com>
2023-10-10 03:23:20 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskLister = document;
+
+  let submitBtn = taskLister.getElementById("create-task-form");
+
+    submitBtn.addEventListener("submit", function (event) {
+      event.preventDefault();
+        let ul = taskLister.getElementById("tasks");
+        let li = taskLister.createElement("li");
+    li.innerHTML = event.target[0].value;
+    ul.appendChild(li);
+    event.target.reset();
+  });
+
+
+
+
+  let deleteBtn = taskLister.getElementById("delete");
+
+  deleteBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let ul = taskLister.getElementById("tasks");
+    let li = ul.lastElementChild;
+    while (li) {
+      ul.removeChild(li);
+      li = ul.lastElementChild;
+    }
+  });
+
+
 });
